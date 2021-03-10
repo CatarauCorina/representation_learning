151884c902f81e571b9019c02beed562e9b378cc
CatarauCorina <corina_catarau@yahoo.ro>
2021-03-10 07:05:26 -0800

    Eval method

--- train_clevr.py
+++ train_clevr.py
@@ -16,31 +16,50 @@ def l2_loss(predicted, target):
     return output
 
 
-def train(model, nr_epochs, optimizer, writer, device):
+def train(model, nr_epochs, optimizer, writer, device, dl, test_dl):
+    i =0
     for e in range(nr_epochs):
         optimizer.zero_grad()
-        train_step(model, optimizer, writer,e, device)
+        i = train_step(model, optimizer, writer,e, device, dl, test_dl, i)
 
 
-def train_step(enc, optimizer, writer, epoch, device):
+def eval(model,test_ds, writer, i):
+    loss = 0
+    iter = 0
+    with torch.no_grad():
+        for i_batch, img_test in enumerate(test_ds):
+            recon_combined, recons, masks, slots = model(img_test)
+            loss_value = l2_loss(recon_combined, img_test)
+            loss += loss_value
+            iter +=10
+    writer.add_scalar('Loss test', loss, i)
 
-    root_dir = os.path.join(os.path.dirname(os.getcwd()), "CLEVR_v1.0\\CLEVR_v1.0\\")
-    df = CustomDataSet(root_dir)
-    dl = DataLoader(dataset=df, batch_size=10)
+
+def train_step(enc, optimizer, writer, epoch, device, dl, test_ds, i):
+    print(epoch)
     for i_batch, sample_batched in enumerate(dl):
-        print(i_batch)
         sample_batched = sample_batched.to(device)
         recon_combined, recons, masks, slots = enc(sample_batched)
         loss_value = l2_loss(recon_combined, sample_batched)/10
         loss_value.backward()
         optimizer.step()
+        writer.add_scalar('Loss iter', loss_value, i)
+        if i % 200 == 0 and i != 0:
+            eval(enc, test_ds, writer, i)
+        i+=1
     writer.add_scalar('Loss train', loss_value, epoch)
     torch.save(enc.state_dict(),f'model_epoch_{epoch}.pth')
-    return
+    return i
 
 
 def main():
-    writer = SummaryWriter(f'results/object_discovery_init')
+    root_dir = os.path.join(os.path.dirname(os.getcwd()), "CLEVR_v1.0\\CLEVR_v1.0\\")
+    df = CustomDataSet(root_dir)
+    train_ds, test_ds = torch.utils.data.random_split(df, [60000, 10000])
+
+    dl_train = DataLoader(dataset=train_ds, batch_size=10)
+    dl_test = DataLoader(dataset=test_ds, batch_size=10)
+    writer = SummaryWriter(f'results/object_discovery_init100')
     device = torch.device('cpu')
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -49,7 +68,7 @@ def main():
     if device == torch.device('cuda'):
         enc.to(device)
     optimizer = optim.Adam(enc.parameters(), lr=0.0004, weight_decay=0.5)
-    train(enc, 1000, optimizer, writer, device)
+    train(enc, 1000, optimizer, writer, device, dl_train,dl_test)
 
 
 if __name__ == '__main__':
